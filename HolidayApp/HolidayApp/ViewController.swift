b6268f4f4d0d6986c52f0f74e03a40a51dd19539
Anderson <tandersonf@hotmail.com>
2023-04-07 08:25:47 -0300

    initial commit

---
+++ HolidayApp/HolidayApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewController.swift
+//  HolidayApp
+//
+//  Created by Anderson Oliveira on 07/04/23.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Loaded")
+    }
+}
+
