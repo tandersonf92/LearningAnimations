073ecb7f8ebb9f03d64130c48b36f24ba95933de
Anderson <tandersonf@hotmail.com>
2023-04-07 10:59:36 -0300

    initial commit with extensions

---
+++ RestaurantApp/RestaurantApp/OnboardingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingViewController.swift
+//  RestaurantApp
+//
+//  Created by Anderson Oliveira on 07/04/23.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+}
+
+// MARK: ViewConfiguration
+extension OnboardingViewController: ViewConfiguration {
+    func configViews() {
+    }
+    
+    func buildViews() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+}
+
